880252a2028e995f88afc5e2686273070ba40a55
Stephen Rifenburgh <Stephen@Rifenburgh.com>
2017-02-24 17:38:11 -0500

    New Product Functionality

--- app.js
+++ app.js
@@ -18,20 +18,22 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const expressLayouts = require('express-ejs-layouts');
 var index = require('./routes/index');
 const productRoutes = require('./routes/products.js');
 //var users = require('./routes/users');  //This Line should be deleted for routes management
 
 mongoose.connect('mongodb://localhost/W4D5L2');
 var app = express();
+app.set('layout', 'layouts/main-layouts');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
+app.use(expressLayouts);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
--- routes/products.js
+++ routes/products.js
@@ -1,13 +1,44 @@
 //Building the Products Routes
 
 const express = require('express');
+const Product = require('../models/products.js');
 const router = express.Router();  //Can be used by as if you were using app.route
 
 router.get('/products', (req, res, next) => {
-  res.send('Products List Page');
+  // res.send('Products List Page');
+  Product.find({},{}, (err, products) => {
+    if (err) {
+      next(err);  //if there is an error go to the 'next' item in the loop
+      return;
+    }
+    //this will display the views/products/index.ejs file
+    res.render('products/index', {
+      products: products
+    });
+  });
 });
 
-router.get('/products/new', (req, res, next) => {});
+//Forms need two routes, one to display a form and another to recieve the data
+router.get('/products/new', (req, res, next) => {
+  res.render('products/new', {});
+});
+
+router.post('/products/new', (req, res, next) => {
+  const productInfo = {
+    name: req.body.name,
+    price: req.body.price,
+    description: req.body.description,
+    imageUrl: req.body.imageUrl
+  };
+  const theProduct = new Product(productInfo);
+  theProduct.save((err) => {
+    if (err) {
+      next (err);
+      return;
+    }
+    res.redirect('/products');
+  });
+});
 
 router.get('/products/remove', (req, res, next) => {});
 
